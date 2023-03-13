9116305058daba284915cf1f4d9778e855799606
pkj <kjiny905217@gmail.com>
2023-03-13 15:41:45 +0900

    초기버전

---
+++ test.js
@@ -0,0 +1,9 @@
+const $btnAdd = document.getElementById('btnAdd');
+$btnAdd.addEventListener('click', () => {
+  console.log('클릭');
+});
+
+const $btnDel = document.getElementById('btnDel');
+$btnDel.addEventListener('cilick', () => {
+  console.log('삭제');
+});
